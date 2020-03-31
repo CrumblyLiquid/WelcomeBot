6c3bd8e41d65b40dff9a1261d2889c8fb10f57a7
CrumblyLiquid <crumblyliquid@gmail.com>
2020-03-31 18:59:39 +0200

    Code added

---
+++ bot.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+from pathlib import Path
+import os
+import json
+import io
+import aiohttp
+from PIL import Image
+
+def getConfig(element: str):
+    with open(str(Path(__file__).parent.absolute()) + '/config.json') as json_file:
+        config = json.load(json_file)
+    return config[element]
+
+bot = commands.Bot(command_prefix = '', help_command=None, description='WelcomeBot is a bot which welcomes everyone!')
+
+@bot.event
+async def on_ready():
+    print('Bot is ready!')
+    print(f'Logged in as {bot.user}')
+    print('')
+    print('Author: CrumblyLiquid (CrumblyLiquid#6668)')
+    print('')
+    print('If you find any bugs please report to:\n- https://github.com/CrumblyLiquid/WelcomeBot\n- crumblyliquid@gmail.com')
+    print('')
+
+@bot.event
+async def on_member_join(member):
+    # Finds channel specified in config.json under element 'channel'
+    for channel in member.guild.channels:
+        if channel.name == getConfig('channel'):
+            async with aiohttp.ClientSession() as session: # Gets the avatar of user
+                async with session.get(str(member.avatar_url_as(size=256))) as response:
+                    if response.status != 200:
+                        print('[ERROR]: Couldn\'t get picture from the avatar url!')
+                        return
+                    data = io.BytesIO(await response.read())
+
+                    # Loads the original avatar picture
+                    avatar = Image.open(data)
+                    # cavatar = Cropped avatar
+                    cavatar = avatar.crop((int((avatar.size[0]-148)/2), int((avatar.size[1]-172)/2), int(avatar.size[0]-((avatar.size[0]-148)/2)), int(avatar.size[1]-((avatar.size[1]-172)/2))))
+                    
+                    # Pastes the avatar into the template.png creating <user_id>.png
+                    template = Image.open(str(Path(__file__).parent.absolute()) + '/template.png')
+                    template.paste(cavatar, (437, 40, 437 + cavatar.size[0], 40 + cavatar.size[1]))
+                    template.paste(cavatar, (441, 381, 441 + cavatar.size[0], 381 + cavatar.size[1]))
+                    template.save(str(Path(__file__).parent.absolute()) + f'/{member.id}.png')
+                    
+                    # Sends the <user_id>.png with the welcoming message
+                    await channel.send(f'Welcome to {member.guild.name}, {member.mention}!', file = discord.File(str(Path(__file__).parent.absolute()) + f'/{member.id}.png'))
+                    
+                    # Deletes the <user_id>.png
+                    os.remove((str(Path(__file__).parent.absolute()) + f'/{member.id}.png'))
+
+bot.run(getConfig('token'), reconnect=True)
